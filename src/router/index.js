c4714e92d0407ec3a621b01319039d1b7a853aae
Lucas Gerisztein <lucas.gerisztein@urbanara.com>
2018-06-27 22:13:30 +0200

    feat: routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import StarredRepos from '@/components/StarredRepos'
 
 Vue.use(Router)
 
@@ -10,7 +11,19 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      props: route => ({
+        date: route.query.date,
+        languages: route.query.langs,
+        order: route.query.order,
+        page: +route.query.page,
+        sort: route.query.sort
+      })
+    },
+    {
+      path: '/starred',
+      name: 'StarredRepos',
+      component: StarredRepos
     }
   ]
 })
